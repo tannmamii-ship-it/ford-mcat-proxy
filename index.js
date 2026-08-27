4935310d69c1c992dd51414b06c8fb1e3992ed43
tannmamii-ship-it <tannmamii@gmail.com>
2026-08-27 18:52:25 +0300

    index.js

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Ford Proxy sunucusu ${PORT} portunda çalışıyor.`);
 });
